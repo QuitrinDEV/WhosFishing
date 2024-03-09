a70cf95d04b5d18d411f195445de9af27b0ccfbb
Quitrindev <87720636+QuitrinDEV@users.noreply.github.com>
2024-03-09 19:07:40 +0300

    Commit

---
+++ src/main/java/net/quitrindev/fishing/WhosFishing.java
@@ -0,0 +1,17 @@
+package net.quitrindev.fishing;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class WhosFishing extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
